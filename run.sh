8c3d749d52fee57801bc1b2d72e1b8a0c28b4078
zbr17 <zhang-br21@mails.tsinghua.edu.cn>
2024-12-05 15:20:31 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+wget https://download.pytorch.org/models/vgg16-397923af.pth -O ./models/hub/checkpoints
+
 # training code
 WORKSPACE=/data1/code/zbr/optvq-gen WANDB_MODE=offline accelerate launch --gpu_ids 0,1,2,3,4,5 --num_machines=1 --num_processes=6 \
 train_maskgit.py config=configs/training/generator/maskgit.yaml \
